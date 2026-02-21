f7d9f3634f61d996c1ce95c63e44f4cc217ca543
Premansh <premansh_ug_23@cse.nits.ac.in>
2026-02-22 00:58:35 +0530

    aws deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const  winston = require('winston');
+const winston = require('winston');
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
@@ -18,8 +18,10 @@ require('./startup/hotelRoutes')(app);
 app.use(error);
 
 mongoose.connect(config.get('dbKey'))
-    .then( () => winston.info('Connected to Database...'));
+    .then(() => winston.info('Connected to Database...'));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port,'0.0.0.0', () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, '0.0.0.0', () => winston.info(`Listening on port ${port}...`));
 module.exports = server;
+
+// hfjklahjfhklah
